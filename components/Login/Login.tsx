07962f608d5bfaf1f19854756abbdce7f5861456
lueenavarro <47080916+lueenavarro@users.noreply.github.com>
2021-04-26 00:58:24 +0800

    Remove google login

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,19 +1,7 @@
 import React from "react";
-import GoogleLogin from "react-google-login";
 
 const Login = () => {
-  const responseGoogle = (response) => {
-    console.log(response, "hello");
-  };
-
-  return (
-    <GoogleLogin
-      clientId="1057006530487-6uei7h2vbuk05mbq9sjqm9q7vv6aqdks.apps.googleusercontent.com"
-      buttonText="Login"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-    />
-  );
+  return <></>
 };
 
 export default Login
